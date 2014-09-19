3b06949443c5549648bd06a2ce6a265069fa6f58
David Laird <kiwiupover@gmail.com>
2014-09-19 10:18:18 -0700

    Adding Divisions

---
+++ website/site/pages/data.js
@@ -0,0 +1,66 @@
+module.exports =  {
+  title: 'Home'
+, documentationUrl: 'http://broccoli-taco.com/documentation/'
+, sponsors: [
+  {
+    name: 'devshot',
+    logo: 'http://www.staticshowdown.com/images/sponsors/divshot.svg',
+    link: 'https://divshot.com/',
+    width: '160'
+  },
+  {
+    name: 'github',
+    logo: 'http://www.staticshowdown.com/images/sponsors/github.svg',
+    link: 'http://www.github.com/',
+    width: '130'
+  },
+  {
+    name: 'firebase',
+    logo: 'http://www.staticshowdown.com/images/sponsors/firebase.svg',
+    link: 'https://www.firebase.com',
+    width: '160'
+  },
+  {
+    name: 'dropbox',
+    logo: 'http://www.staticshowdown.com/images/sponsors/dropbox.svg',
+    link: 'http://www.dropbox.com/',
+    width: '160'
+  },
+  {
+    name: 'Sticker Mule',
+    logo: 'http://www.staticshowdown.com/images/sponsors/stickermule.svg',
+    link: 'http://www.stickermule.com/',
+    width: '200'
+  },
+  {
+    name: 'devshot',
+    logo: 'http://www.staticshowdown.com/images/sponsors/divshot.svg',
+    link: 'https://divshot.com/',
+    width: '160'
+  },
+  {
+    name: 'github',
+    logo: 'http://www.staticshowdown.com/images/sponsors/github.svg',
+    link: 'http://www.github.com/',
+    width: '130'
+  },
+  {
+    name: 'firebase',
+    logo: 'http://www.staticshowdown.com/images/sponsors/firebase.svg',
+    link: 'https://www.firebase.com',
+    width: '160'
+  },
+  {
+    name: 'dropbox',
+    logo: 'http://www.staticshowdown.com/images/sponsors/dropbox.svg',
+    link: 'http://www.dropbox.com/',
+    width: '160'
+  },
+  {
+    name: 'Sticker Mule',
+    logo: 'http://www.staticshowdown.com/images/sponsors/stickermule.svg',
+    link: 'http://www.stickermule.com/',
+    width: '200'
+  }
+]
+};
